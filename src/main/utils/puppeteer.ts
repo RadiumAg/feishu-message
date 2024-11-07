a292d1f651018832ab3f56684aa83455606b3d69
张镭银 <zly01858966@lazada.com>
2024-11-07 15:07:34 +0800

    feat: add user interaction in puppeteer script

--- src/main/utils/puppeteer.ts
+++ src/main/utils/puppeteer.ts
@@ -1,13 +1,30 @@
 import puppeteer from 'puppeteer';
 
+const userName = 'Radium';
+
 const runPuppeteer = async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    args: [''],
+    defaultViewport: null,
   });
   const page = await browser.newPage();
-  await page.goto('https://eabxwfafdhx.feishu.cn');
-  page.reload();
+  await page.goto('https://ezeb4r28vm.feishu.cn/next/messenger');
+  await page
+    .locator('[data-feed-id="7433401383485063169"] .a11y_feed_card_item')
+    .click();
+
+  setTimeout(async () => {
+    const messageListElement = await page.locator(
+      '.messageList-row-wrapper:last-child',
+    )._;
+    const elementUserName = await page.locator(
+      '.messageList-row-wrapper:last-child message-info-name',
+    )._?.textContent;
+
+    if (elementUserName === userName) {
+      messageListElement.
+    }
+  }, 1000);
 };
 
 export { runPuppeteer };
