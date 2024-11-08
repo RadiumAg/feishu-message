cae402a3453036a594b881874a40654ac4027f79
张镭银 <zly01858966@lazada.com>
2024-11-08 15:09:50 +0800

    refactor: enhance puppeteer script with event listeners and resize observer

--- src/main/utils/puppeteer.ts
+++ src/main/utils/puppeteer.ts
@@ -13,17 +13,37 @@ const runPuppeteer = async () => {
     .locator('[data-feed-id="7433401383485063169"] .a11y_feed_card_item')
     .click();
 
-  setTimeout(async () => {
-    const messageListElement = await page.locator(
-      '.messageList-row-wrapper:last-child',
-    )._;
-    const elementUserName = await page.locator(
-      '.messageList-row-wrapper:last-child message-info-name',
-    )._?.textContent;
+  await page.evaluate(() => {
+    const event = new Event('listen-message', {
+      bubbles: true,
+      cancelable: true,
+    });
+    document.dispatchEvent(event);
+  });
 
-    if (elementUserName === userName) {
-    }
-  }, 1000);
+  // 监听messageList改变
+  await page.evaluate(() => {
+    const sizeObserver = new ResizeObserver(async () => {
+      const messageListElement = await page.locator(
+        '.messageList-row-wrapper:last-child',
+      )._;
+      const elementUserName = await page.locator(
+        '.messageList-row-wrapper:last-child message-info-name',
+      )._?.textContent;
+
+      if (elementUserName === userName) {
+      }
+
+      console.log(messageListElement);
+    });
+    const messageListElement = document.querySelector('.messageList');
+
+    if (messageListElement == null) return;
+
+    sizeObserver.observe(messageListElement);
+  });
+
+  setTimeout(async () => {}, 1000);
 };
 
 export { runPuppeteer };
