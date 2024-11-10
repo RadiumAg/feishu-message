c5da4cb6de5d2f401b0b42201504fd1f759a8c12
Radium <2043869032@qq.com>
2024-11-10 17:21:36 +0800

    feat(puppeteer): improve message listening functionality

--- src/main/utils/puppeteer.ts
+++ src/main/utils/puppeteer.ts
@@ -1,51 +1,53 @@
 import puppeteer from 'puppeteer';
 
 const userName = 'Radium';
+const listenMessageEventName = 'listen-message';
+
+const checkSendName = (sendUserName: string, node: Node) => {
+  while (userName !== sendUserName &&  node.c) {}
+};
 
 const runPuppeteer = async () => {
   const browser = await puppeteer.launch({
     headless: false,
+    devtools: true,
     defaultViewport: null,
+    args: ['--remote-debugging-port=9222'],
   });
+
   const page = await browser.newPage();
   await page.goto('https://ezeb4r28vm.feishu.cn/next/messenger');
   await page
     .locator('[data-feed-id="7433401383485063169"] .a11y_feed_card_item')
     .click();
 
-  await page.evaluate(() => {
-    const event = new Event('listen-message', {
-      bubbles: true,
-      cancelable: true,
-    });
-    document.dispatchEvent(event);
+  // 使用page.exposeFunction()向浏览器发送事件回调
+  await page.exposeFunction(listenMessageEventName, (message) => {
+    console.log(message);
   });
 
   // 监听messageList改变
   await page.evaluate(() => {
-    const sizeObserver = new MutationObserver(async () => {
-      const messageListElement = await page.locator(
-        '.messageList-row-wrapper:last-child',
-      )._;
-      const elementUserName = await page.locator(
-        '.messageList-row-wrapper:last-child message-info-name',
-      )._?.textContent;
-
-      if (elementUserName === userName) {
+    const mutationObserver = new MutationObserver((mutationsList) => {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const mutation of mutationsList) {
+        const addNodes = mutation.addedNodes;
+        addNodes.forEach((node) => {
+          node.previousSibling;
+        });
       }
 
-      console.log(messageListElement);
+      window['listen-message']();
     });
-    const messageListElement = document.querySelector('.messageList');
 
+    const messageListElement = document.querySelector('.messageList');
     if (messageListElement == null) return;
 
-    sizeObserver.observe(messageListElement, {
+    mutationObserver.observe(messageListElement, {
       childList: true,
+      subtree: true,
     });
   });
-
-  setTimeout(async () => {}, 1000);
 };
 
 export { runPuppeteer };
