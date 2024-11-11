4f14e3bd2cb95c1e8fae9ae6de2ab40f28477717
张镭银 <zly01858966@lazada.com>
2024-11-11 11:47:51 +0800

    refactor: update puppeteer import and add message listener function

--- src/main/utils/puppeteer.ts
+++ src/main/utils/puppeteer.ts
@@ -1,4 +1,4 @@
-import puppeteer from 'puppeteer';
+import puppeteer, { Page } from 'puppeteer';
 
 const userName = 'Radium';
 const listenMessageEventName = 'listenMessage';
@@ -13,54 +13,18 @@ type RichDocMessage = {
 
 type MessageType = 'text-only' | 'rich-message' | 'image-only';
 
-const getMessageType = (message: HTMLElement): MessageType | undefined => {
-  if (message.classList.contains('.richTextDocs')) return 'rich-message';
-  if (message.classList.contains('.richTextContainer')) return 'text-only';
-  if (message.classList.contains('.img-container')) return 'image-only';
-  return undefined;
-};
-
-const createMessageObject = (
-  message: HTMLElement,
-): ImageMessage | TextMessage | undefined => {
-  if (message.classList.contains('text-only')) {
-    return {
-      tag: 'text',
-      text: message.innerText,
-      style: [],
-    };
-  }
-  if (message.classList.contains('img-container')) {
-    const img = message.querySelector('img');
-    if (img == null) return undefined;
-    return {
-      tag: 'img',
-      image_key: img.src,
-    };
-  }
-
-  return undefined;
-};
-
-const runPuppeteer = async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    devtools: true,
-    defaultViewport: null,
-    args: ['--remote-debugging-port=9222'],
-  });
-
-  const page = await browser.newPage();
-  await page.goto('https://ezeb4r28vm.feishu.cn/next/messenger');
-  await page
-    .locator('[data-feed-id="7433401383485063169"] .a11y_feed_card_item')
-    .click();
-
-  // 使用page.exposeFunction()向浏览器发送事件回调
+/**
+ * 监听消息列表
+ *
+ * @param {Page} page
+ */
+const evaluateListenMessaggee = async (page: Page) => {
+  // 使用page.exposeFunction()监听浏览器向nodejs发送事件回调
   await page.exposeFunction(listenMessageEventName, (message) => {
     console.log(message);
   });
 
+  // 等待messageList加载完成
   await page.waitForSelector('.messageList');
 
   // 监听messageList改变
@@ -68,13 +32,65 @@ const runPuppeteer = async () => {
     let sendMessageObject = {};
     const messageListElement = document.querySelector('.messageList');
 
+    const getMessageType = (message: HTMLElement): MessageType | undefined => {
+      if (message.classList.contains('.richTextDocs')) return 'rich-message';
+      if (message.classList.contains('.richTextContainer')) return 'text-only';
+      if (message.classList.contains('.img-container')) return 'image-only';
+      return undefined;
+    };
+
+    const createMessageObject = (
+      message: HTMLElement,
+    ): ImageMessage[] | TextMessage[] | undefined => {
+      if (message.classList.contains('rich-text-paragraph')) {
+        const textElementObject: TextMessage[] = [];
+
+        const textElementArray = message.querySelectorAll('.text-only');
+        textElementArray.forEach((textElement) => {
+          if (textElement == null) return;
+
+          textElementObject.push({
+            tag: 'text',
+            text: message.innerText,
+            style: [],
+          });
+        });
+
+        return textElementObject;
+      }
+      if (message.classList.contains('rich-text-image')) {
+        const imageObjectArray: ImageMessage[] = [];
+        const imgElementArray = message.querySelectorAll('img');
+
+        imgElementArray.forEach((imgElement) => {
+          if (imgElement == null) return;
+
+          imageObjectArray.push({
+            tag: 'img',
+            image_key: imgElement.src,
+          });
+        });
+
+        return imageObjectArray;
+      }
+
+      return undefined;
+    };
+
     const mutationObserver = new MutationObserver((mutationsList) => {
       // eslint-disable-next-line no-restricted-syntax
       for (const mutation of mutationsList) {
         const messageWrapper = mutation.target as HTMLElement;
 
         if (messageWrapper.classList.contains('messageItem-wrapper')) {
-          const messageType = getMessageType(messageWrapper);
+          const richDocElement = messageWrapper.querySelector(
+            '.richTextContainer > *',
+          );
+          console.log('richDocElement', richDocElement);
+          if (richDocElement == null) return;
+          const messageType = getMessageType(richDocElement as HTMLElement);
+          console.log('messageType', messageType);
+
           sendMessageObject = {
             text: '',
             content: [],
@@ -101,7 +117,7 @@ const runPuppeteer = async () => {
                 const contentItem = createMessageObject(element as HTMLElement);
                 if (contentItem)
                   (sendMessageObject as RichDocMessage).content.push(
-                    contentItem,
+                    ...contentItem,
                   );
               }
 
@@ -133,6 +149,23 @@ const runPuppeteer = async () => {
       subtree: true,
     });
   });
+};
+
+const runPuppeteer = async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    devtools: true,
+    defaultViewport: null,
+    args: ['--remote-debugging-port=9222'],
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://ezeb4r28vm.feishu.cn/next/messenger');
+  await page
+    .locator('[data-feed-id="7433401383485063169"] .a11y_feed_card_item')
+    .click();
+
+  await evaluateListenMessaggee(page);
 };
 
 export { runPuppeteer };
